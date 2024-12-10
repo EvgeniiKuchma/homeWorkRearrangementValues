d6803d7b2d5b4c16214b2d4d5254f958ead572fa
Евгений Кучма <desferto87@gmail.com>
2024-12-10 13:47:41 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkRearrangementValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Cage";
+            string surename = "Anna";
+            string cupTea = "coffe";
+            string cupCoffe = "tea";
+            string temp;
+
+            Console.WriteLine($"Name is {name}, surename is {surename}");
+            Console.WriteLine($"In the cup of tea --> {cupTea}, in the cup of coffee --> {cupCoffe}");
+
+            temp = surename;
+            surename = name;
+            name = temp;
+            temp = cupTea;
+            cupTea = cupCoffe;
+            cupCoffe = temp;
+
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine($"Name is {name}, surename is {surename}");
+            Console.WriteLine($"In the cup of tea --> {cupTea}, in the cup of coffee --> {cupCoffe}");
+        }
+    }
+}
